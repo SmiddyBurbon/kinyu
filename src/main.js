fd8a802e57f66ce4125fd3ac85362e69685da8f4
Unknown <me@dennisschmidt.net>
2020-03-29 15:05:42 +0200

    installed vue-router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import VueHtml2Canvas from 'vue-html2canvas';
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import '@babel/polyfill'
 import "regenerator-runtime/runtime";
 import "canvas-toBlob";
-import $ from 'jquery'
 
 Vue.use(VueHtml2Canvas);
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
